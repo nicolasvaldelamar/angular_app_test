e7a573083eaa3bb2d065dc224f2cf350af88689b
nicolasvaldelamar <nicolasvaldelamar.c@gmail.com>
2023-05-18 12:48:57 -0500

    Empezamos el viaje

---
+++ angular_app_test/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular_app_test',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
